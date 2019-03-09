eeaa0c2039c387d2686ee9b700903e215d4381f3
szotaa <kubaszota97@gmail.com>
2019-03-09 18:57:07 +0100

    Initial application

---
+++ backend/src/main/java/online/ekingdom/EkingdomApplication.java
@@ -0,0 +1,13 @@
+package online.ekingdom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EkingdomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EkingdomApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/online/ekingdom/EkingdomApplicationTests.java
@@ -0,0 +1,16 @@
+package online.ekingdom;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EkingdomApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
